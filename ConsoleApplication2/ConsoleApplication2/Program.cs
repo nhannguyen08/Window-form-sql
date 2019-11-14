641d57af995648a5efe5823c7c84594cb506845f
Nguyễn Thành Nhân <nhocksok1009@gmail.com>
2019-11-14 17:35:38 +0700

    Form

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var md = new OOPCSEntities();
+            Expenditure[] list = md.Expenditures.ToArray();
+            
+            int sum = 0;
+            for (int i = 0; i < list.Length; i++)
+            {
+                Console.WriteLine(list[i].id + " " + list[i].Note +" "+list[i].Price+ " " + list[i].Date);
+                
+                sum += list[i].Price;
+            }
+            Console.WriteLine("Tong gia tien la :{0}đ", sum);
+
+        }
+    }
+}
